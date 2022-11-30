5e889a6c4a201a564f00df39fcd61200769ae885
044_Alda Ellsa <114373409+aldaellsa@users.noreply.github.com>
2022-11-30 12:45:32 +0700

    Add files via upload

---
+++ CRUD Framework Laravel/Tugas6/app/Models/Mahasiswa.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Mahasiswa extends Model
+{
+    use HasFactory;
+    public $table = "mahasiswa";
+    protected $primaryKey = 'id';
+    protected $fillable = ['nim', 'nama', 'jk', 'alamat', 'prodi', 'foto', 'email'];
+}
